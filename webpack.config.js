90dbc1d0e806f1bef15d6fb8f267e3f97ddae597
Lewis Robinson <lewisvrobinson@gmail.com>
2019-01-04 16:20:48 +1100

    setup loaders and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,45 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve('./static')
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
       }
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve('./static'),
+    compress: true,
+    port: 9000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/api'
+      }
+    }
   }
 };
