306c66748da95e446837f6d46844a3c515723d89
Lewis Robinson <lewisvrobinson@gmail.com>
2019-01-06 23:14:11 +1100

    global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import AddCounter from './components/AddCounter'
 import CounterList from './components/CounterList'
+import styled from 'styled-components'
 
+const Wrapper = styled.div`
+  margin: 4em auto 0 auto;
+  width: 600px;
+  max-width: 100%;
+  padding: 1em;
+  background: #fff;
+`
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper>
         <AddCounter />
         <CounterList />
-      </div>
+      </Wrapper>
     )
   }
 }
